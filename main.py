289aa9e5d1467309cff40a5c78320f848c964804
ppaka <asj0216@gmail.com>
2022-01-22 04:47:15 +0900

    웹 크롤링 코드 최적화

--- main.py
+++ main.py
@@ -18,14 +18,8 @@ def cfDecodeEmail(encodedString):
     return email
 
 
-class TargetFormat(object):
-    GIF = ".gif"
-    MP4 = ".mp4"
-    AVI = ".avi"
-
-
-def convertFile(inputpath, outputdir, outputfilename, targetFormat):
-    outputpath = outputdir + outputfilename + targetFormat
+def convertFile(inputpath, outputdir, outputfilename):
+    outputpath = outputdir + outputfilename + '.gif'
 
     reader = imageio.get_reader(inputpath)
     fps = reader.get_meta_data()['fps']
@@ -36,24 +30,24 @@ def convertFile(inputpath, outputdir, outputfilename, targetFormat):
     writer.close()
 
 
-print('---- 깃허브: https://github.com/ppaka/ArcaliveEmojiDownloader ----')
+def main():
+    print('---- 깃허브: https://github.com/ppaka/ArcaliveEmojiDownloader ----')
 
-path = os.path.dirname(os.path.abspath(__file__))
-url = input('아카콘 페이지 주소 입력: ')
+    path = os.path.dirname(os.path.abspath(__file__))
+    url = input('아카콘 페이지 주소 입력: ')
 
-req = requests.get(url)
-soup = BeautifulSoup(req.text, "html.parser")
+    req = requests.get(url)
+    soup = BeautifulSoup(req.text, "html.parser")
 
-title_element = soup.select_one(
-    'body > div.root-container > div.content-wrapper.clearfix > article > div > div.article-wrapper > div.article-head > div.title-row > div')
+    title_element = soup.select_one(
+        'body > div.root-container > div.content-wrapper.clearfix > article > div > div.article-wrapper > div.article-head > div.title-row > div')
 
-title = str(title_element).splitlines()[1]
-for j in title:
+    title = str(title_element).splitlines()[1]
+    
     if '<a class="__cf_email__"' in title:
-        print(title)
+        print('제목에 암호화된 기호가 있습니다. 변환 작업을 진행합니다.')
         start_index = title.find('<a class="__cf_email__"')
         end_index = title.find('</a>')+4
-        print(end_index)
         
         email_source = title[start_index:end_index]
 
@@ -65,64 +59,76 @@ for j in title:
 
         title = title[:start_index] + email + title[end_index:]
 
-while True:
-    if title.endswith(' '):
+    if title.endswith(' ') or title.startswith(' '):
         print('제목 맨 끝에 공백이 존재합니다. 공백을 제거합니다.')
-        title = title[0:-1]
-    else:
-        break
-print('다음을 다운로드합니다...', '"'+title+'"')
+        title.strip()
 
-if re.search('[\/:*?"<>|]', title):
-    old_title = title
-    title = re.sub('[\/:*?"<>|]', '', title)
-    print(f'제목에 사용 불가능한 문자가 있어 폴더이름을 변경하여 저장합니다.\n{old_title} ▶▶ {title}')
+    print('다음을 다운로드합니다...', '"'+title+'"')
 
-element = soup.select_one(
-    'body > div > div.content-wrapper.clearfix > article > div > div.article-wrapper > div.article-body > div')
+    if re.search('[\/:*?"<>|]', title):
+        old_title = title
+        title = re.sub('[\/:*?"<>|]', '', title)
+        print(f'제목에 사용 불가능한 문자가 있어 폴더이름을 변경하여 저장합니다.\n{old_title} ▶▶ {title}')
 
-arcacon = str(element).splitlines()
-if arcacon[0] == '<div class="emoticons-wrapper">':
-    arcacon.pop(0)
-    arcacon.pop(len(arcacon)-1)
-    for i in range(len(arcacon)):
-        arcacon[i] = arcacon[i].replace('<img loading="lazy" src="', '')
-        arcacon[i] = arcacon[i].replace('"/>', '')
+    element = soup.select_one(
+        'body > div > div.content-wrapper.clearfix > article > div > div.article-wrapper > div.article-body > div')
 
-        arcacon[i] = arcacon[i].replace(
-            '<video autoplay="" loading="lazy" loop="" muted="" playsinline="" src="', '')
-        arcacon[i] = arcacon[i].replace('"></video>', '')
-        arcacon[i] = 'https:'+arcacon[i]
+    total_count = 0
 
-count = 0
+    for i in element.find_all('video'):
+        total_count+=1
+        #print(i['src'])
+    for i in element.find_all('img'):
+        total_count+=1
+        #print(i['src'])
 
-for i in arcacon:
-    count += 1
-    sav_dir = path.replace('\\', '/')+'/'+title+'/'
+    print('찾은 아카콘 개수: ' + str(total_count))
 
-    if not os.path.exists(sav_dir):
-        os.makedirs(sav_dir)
+    arcacon = str(element).splitlines()
 
-    vid_dir = sav_dir+'/videos/'
-    if not os.path.exists(vid_dir):
-        os.makedirs(vid_dir)
+    if arcacon[0] == '<div class="emoticons-wrapper">':
+        arcacon = arcacon[1:total_count+1]
+        for i in range(len(arcacon)):
+            arcacon[i] = arcacon[i].replace('<img loading="lazy" src="', '')
+            arcacon[i] = arcacon[i].replace('"/>', '')
 
-    filename = str(count)
-    if i.endswith('.png'):
-        filename += '.png'
-        download(i, sav_dir+filename)
-    elif i.endswith('.jpeg'):
-        filename += '.jpeg'
-        download(i, sav_dir+filename)
-    elif i.endswith('.jpg'):
-        filename += '.jpg'
-        download(i, sav_dir+filename)
-    elif i.endswith('.gif'):
-        filename += '.gif'
-        download(i, sav_dir+filename)
-    elif i.endswith('.mp4'):
-        filename += '.mp4'
-        download(i, vid_dir+filename)
-        convertFile(vid_dir+filename, sav_dir, str(count), TargetFormat.GIF)
+            arcacon[i] = arcacon[i].replace(
+                '<video autoplay="" loading="lazy" loop="" muted="" playsinline="" src="', '')
+            arcacon[i] = arcacon[i].replace('"></video>', '')
+            arcacon[i] = 'https:'+arcacon[i]
 
-print('----다운로드를 마쳤습니다----')
+    count = 0
+
+    for i in arcacon:
+        count += 1
+        sav_dir = path.replace('\\', '/')+'/'+title+'/'
+
+        if not os.path.exists(sav_dir):
+            os.makedirs(sav_dir)
+
+        vid_dir = sav_dir+'/videos/'
+        if not os.path.exists(vid_dir):
+            os.makedirs(vid_dir)
+
+        filename = str(count)
+        if i.endswith('.png'):
+            filename += '.png'
+            download(i, sav_dir+filename)
+        elif i.endswith('.jpeg'):
+            filename += '.jpeg'
+            download(i, sav_dir+filename)
+        elif i.endswith('.jpg'):
+            filename += '.jpg'
+            download(i, sav_dir+filename)
+        elif i.endswith('.gif'):
+            filename += '.gif'
+            download(i, sav_dir+filename)
+        elif i.endswith('.mp4'):
+            filename += '.mp4'
+            download(i, vid_dir+filename)
+            convertFile(vid_dir+filename, sav_dir, str(count))
+
+    print('---- 다운로드를 마쳤습니다 ----')
+
+if __name__=='__main__':
+    main()
